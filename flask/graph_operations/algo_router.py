d1721dff9dcd35142a3dbec4d629f50c19fcf493
Sohaib Manah <souhaibemanah@gmail.com>
2023-05-17 23:53:36 +0100

    all aglo are added todo fine a way to represente them as agraph

--- flask/graph_operations/algo_router.py
+++ flask/graph_operations/algo_router.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import networkx as nx
+
 from algorithms.breadth_first_search import bfs_edges
 from algorithms.depth_first_search import dfs_edges
 from algorithms.dijkstra import dijkstra_path
@@ -10,22 +10,26 @@ from algorithms.dijkstra import dijkstra_path
 
 
 def algo_router(graph, algo, body):
+    print(algo)
     out_put = None
     match algo:
         case "bfs":
             out_put = list(bfs_edges(graph, body["start"]))
-
         case "dfs":
             out_put = list(dfs_edges(graph, body["start"]))
         case "dijkstra":
-            path = nx.dijkstra_path(graph, body["start"], body["target"], weight="weight")
+            path = dijkstra_path(graph, body["start"], body["target"], weight="weight")
             out_put = list(zip(path, path[1:]))
-        # case "bellman-ford":
-        # out_put = list(nx.bellman_ford_path(graph, body["start"], body["end"]))
-        # case "kosaraju":
-        # out_put = max(kosaraju_strongly_connected_components(graph), key=len)
-        # case "prime":
-        # out_put = nx.minimum_spanning_edges(graph)
+        case "bellmanFord":
+            out_put = list(nx.single_source_bellman_ford(graph, body["start"], weight="weight"))
+
+        case "floydWarshall":
+            out_put = list(nx.floyd_warshall(graph, weight="weight"))
+        #     todo find a way to return the path as a graph
+        case "kosaraju":
+            out_put = max(nx.kosaraju_strongly_connected_components(graph), key=len)
+        case "prime":
+            out_put = nx.minimum_spanning_edges(graph)
         # case _:
         #     raise {"status": "error", "message": "Algorithm not supported!"}
     # clearing the original graph to draw another graph
